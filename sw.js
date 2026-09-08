3fd9faa5c35f73db70c0f0306e626c6d37167076
Leonardo Vitulli <leovitulli@Leonardos-MacBook-Pro.local>
2026-09-08 13:57:10 -0300

    fix(ui): corrigir renderizacao do icone de sininho SVG nativo no header

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Garante funcionamento 100% Offline em Smartphones e Tablets nos shows.
  */
 
-var CACHE_NAME = 'canta-ai-pro-v138';
+var CACHE_NAME = 'canta-ai-pro-v139';
 var ASSETS = [
   './',
   './index.html',
