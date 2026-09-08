a80649c5e76c633d2a29fbe2d9e020492c4c00a6
Leonardo Vitulli <leovitulli@Leonardos-MacBook-Pro.local>
2026-09-07 21:20:45 -0300

    style(prompter): substituir cor verde do botao voltar ao topo por pill dark glass enterprise

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Garante funcionamento 100% Offline em Smartphones e Tablets nos shows.
  */
 
-var CACHE_NAME = 'canta-ai-pro-v126';
+var CACHE_NAME = 'canta-ai-pro-v127';
 var ASSETS = [
   './',
   './index.html',
